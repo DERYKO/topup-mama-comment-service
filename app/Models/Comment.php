2d5ddf28bc5a03e1ca67828c347ccb351f40438d
Derrick <derrick@solutech.co.ke>
2022-03-29 09:43:04 +0300

    read me

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,19 @@
 namespace App\Models;
 
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $fillable = [
+        'isbn',
+        'comment',
+        'ip_address'
+    ];
 
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->toDayDateTimeString();
+    }
 }
